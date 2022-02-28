609588a21d99a0d1bfc3be95fd5beeafff45c1bc
kosher <lionel.kossi@gmail.com>
2022-02-28 23:32:26 +0000

    Add click event on mobile menu

---
+++ mobile-menu.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById('menu-button')
+const menuList = document.getElementById('menu-list')
+const menuClose = document.getElementById('menu-close')
+
+menuBtn.addEventListener('click', () => {
+    menuList.style.display = 'flex'
+})
+
+menuClose.addEventListener('click', () => {
+    menuList.style.display = 'none'
+})
