6fe86cf65819ad30cd5771d513091c2a14c3f361
kosher <lionel.kossi@gmail.com>
2022-02-28 23:53:18 +0000

    Add anchor link to menu items

--- mobile-menu.js
+++ mobile-menu.js
@@ -2,6 +2,10 @@ const menuBtn = document.getElementById('menu-button');
 const menuList = document.getElementById('menu-list');
 const menuClose = document.getElementById('menu-close');
 
+const itemPort = document.getElementById('item-portfolio');
+const itemAbout = document.getElementById('item-about');
+const itemContact = document.getElementById('item-contact');
+
 menuBtn.addEventListener('click', () => {
   menuList.style.display = 'flex';
 });
@@ -9,3 +13,15 @@ menuBtn.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
   menuList.style.display = 'none';
 });
+
+itemPort.addEventListener('click', () => {
+    menuList.style.display = 'none';
+})
+
+itemAbout.addEventListener('click', () => {
+    menuList.style.display = 'none';
+})
+
+itemContact.addEventListener('click', () => {
+    menuList.style.display = 'none';
+})
