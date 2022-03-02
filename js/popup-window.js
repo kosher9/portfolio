9ed89355bae6f04f2e123d8294f601cb5f93342e
maen <maen.1980@yahoo.com>
2022-03-02 16:59:57 +0300

    modal popup window
    add modal window to index.html file
    add css code to style the modal
    add javascript code to open and close the modal

--- js/popup-window.js
+++ js/popup-window.js
@@ -60,17 +60,20 @@ function insertAfter(referenceNode, newNode) {
 }
       
 
+
 window.addEventListener('load', (event) => { 
-    
+
     createDom()
 
 })
 
+
+
+let bigSection = document.createElement('section');
 function createDom(){
 
     var hero = document.getElementById('hero');
 
-    let bigSection = document.createElement('section');
     bigSection.className = 'grid-project'
 
     for(let project in projects){
@@ -133,7 +136,6 @@ function createDom(){
 
         for(let tech in technologies){
             let lg = document.createElement('li')
-            console.log(tech)
             if(technologies[tech] == 'Ruby on rails'){
                 lg.className = 'prjct-lg rb'
             } else {
@@ -149,8 +151,20 @@ function createDom(){
         let button = document.createElement('button')
         button.className = 'prjct btn see-project'
         button.type = 'button'
+        button.id = 'btnSeeProject'
         button.innerHTML = 'See Project'
 
+        /* this is the popup window (modal) */
+        button.addEventListener('click', () => {
+           const modal = document.querySelector('.modal-container');
+  
+           const title = document.querySelector('.prjct-name');
+           title.textContent = projects[project].name;
+           modal.style.display = 'block';   
+            
+        })
+    
+   
 
         // Append element to parent
 
@@ -177,116 +191,17 @@ function createDom(){
 
         divInfo.appendChild(buttonContainer)
         buttonContainer.appendChild(button)
+
     }
 
     insertAfter(hero, bigSection)
 
 }
 
-// let section = document.createElement('section')
-    //     section.className = 'project-container-1 ct1'
+const btnClose = document.querySelector('.modal-close-btn'); 
+const modal = document.querySelector('.modal-container');
+btnClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+    
+})
 
-    //     let divContainer = document.createElement('div')
-    //     divContainer.className = 'img-1-container'
-
-    //     let img = document.createElement('img')
-    //     img.className = 'img-size'
-    //     img.src = "img/Portfolio_1.png"
-
-    //     let divInfo = document.createElement('div')
-    //     divInfo.className = 'prjt-info'
-
-    //     let projectTitle = document.createElement('h2')
-    //     projectTitle.className = 'prjct left'
-    //     projectTitle.textContent='Tonic'
-
-    //     let divGridContainer = document.createElement('div')
-    //     divGridContainer.className = 'prjct grid-container'
-        
-    //     let companyName = document.createElement('h4')
-    //     companyName.className = 'prjct-name'
-    //     companyName.textContent = 'CANOPY'
-
-    //     let dotContainer = document.createElement('div')
-    //     dotContainer.className = 'dot-container'
-
-    //     let dot = document.createElement('img')
-    //     dot.className = 'dot'
-    //     dot.src = 'img/counter.png'
-        
-    //     let role = document.createElement('h4')
-    //     role.className = 'prjct-role'
-    //     role.textContent = 'Back End Dev'
-
-    //     let scDotContainer = document.createElement('div')
-    //     scDotContainer.className = 'dot-container'
-
-    //     let scDot = document.createElement('img')
-    //     scDot.className = 'dot'
-    //     scDot.src = 'img/counter.png'
-
-    //     let year = document.createElement('h4')
-    //     year.className = 'prjct-year'
-    //     year.textContent = '2015'
-
-    //     let description = document.createElement('p')
-    //     description.className = 'prjct prjct-descr'
-    //     description.textContent = 'A daily selection of privately personalized reads; no accounts or sign-ups required.'
-
-    //     let techs = document.createElement('ul')
-    //     techs.className = 'prjct container-lg'
-        
-    //     let techOne = document.createElement('li')
-    //     techOne.className = 'prjct-lg'
-    //     techOne.textContent = 'Love'
-
-    //     let techTwo = document.createElement('li')
-    //     techTwo.className = 'prjct-lg'
-    //     techTwo.textContent = 'Love'
-
-    //     let techThree = document.createElement('li')
-    //     techThree.className = 'prjct-lg'
-    //     techThree.textContent = 'Love'
-
-    //     let techFour = document.createElement('li')
-    //     techThree.className = 'prjct-lg'
-    //     techThree.textContent = 'Love'
-
-    //     let buttonContainer = document.createElement('div')
-    //     buttonContainer.className = 'btn-container'
-
-    //     let button = document.createElement('button')
-    //     button.className = 'prjct btn see-project'
-    //     button.type = 'button'
-    //     button.innerHTML = 'See Project'
-
-
-    //     // Append element to parent
-
-    //     bigSection.appendChild(section)
-    //     section.appendChild(divContainer)
-    //     divContainer.appendChild(img)
-
-    //     section.appendChild(divInfo)
-
-    //     divInfo.appendChild(projectTitle)
-    //     divInfo.appendChild(divGridContainer)
-
-    //     divGridContainer.appendChild(companyName)
-    //     divGridContainer.appendChild(dotContainer)
-    //     dotContainer.appendChild(dot)
-    //     divGridContainer.appendChild(role)
-    //     divGridContainer.appendChild(scDotContainer)
-    //     scDotContainer.appendChild(scDot)
-    //     divGridContainer.appendChild(year)
-
-    //     divInfo.appendChild(description)
-
-    //     divInfo.appendChild(techs)
-    //     techs.appendChild(techOne)
-    //     techs.appendChild(techTwo)
-    //     techs.appendChild(techThree)
-    //     techs.appendChild(techFour)
-
-    //     divInfo.appendChild(buttonContainer)
-    //     buttonContainer.appendChild(button)
