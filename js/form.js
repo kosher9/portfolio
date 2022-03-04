1e5a585d44e90aad7f97ef3eaefe24ca28fe9f7c
kosher <lionel.kossi@gmail.com>
2022-03-04 01:05:31 +0000

    -Add a form validation condition

---
+++ js/form.js
@@ -0,0 +1,72 @@
+const email = document.getElementById("email")
+const form = document.querySelector("form")
+const error = document.querySelector(".error")
+let errorState = "valid"
+
+function addEvent(element, event, callback) {
+    let previousEventCallBack = element["on"+event];
+    element["on"+event] = function (e) {
+      const output = callback(e);
+  
+      // A callback that returns `false` stops the callback chain
+      // and interrupts the execution of the event callback.
+      if (output === false) return false;
+  
+      if (typeof previousEventCallBack === 'function') {
+        output = previousEventCallBack(e);
+        if(output === false) return false;
+      }
+    }
+}
+
+function hasUpperCase(str) {
+    return (/[A-Z]/.test(str));
+}
+
+function checkEmailField(){
+    const test = hasUpperCase(email.value) || email.value.length == 0
+    if(test){
+        if (email.value.length == 0){
+            errorState = "empty"
+        } else {
+            errorState = "invalid"
+        }
+    } else {
+        errorState = "valid"
+    }
+    console.log(errorState)
+}
+
+function validateForm(){
+
+    checkEmailField()
+    if(errorState != "valid"){
+        if(errorState == "invalid"){
+            error.textContent = "Email is invalid. Should only use lower caracter."
+        } else {
+            error.textContent = "Please fill the email field!"
+        }
+        console.log("false")
+        return false
+        
+    } else {
+        console.log("true")
+        return true
+    }
+}
+
+// This defines what happens when the user types in the field
+addEvent(email, "input", function(){
+    checkEmailField()
+})
+
+// This defines what happens when the user tries to submit the data
+function submitForm(event){
+
+    if(validateForm() === false){
+        event.preventDefault();
+        window.history.back();
+    }
+
+}
+
